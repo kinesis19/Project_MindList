445e6eb17dddbaf9c81d4beb2a9a2b74fb266a4a
Kinesis <lordkinesis@naver.com>
2024-08-29 02:21:52 +0900

    :hammer: Create robot-mode-rescue.c and robot-mode-rescue.h

---
+++ ProjectMindeList/MindeList/robot-mode-rescue.c
@@ -0,0 +1,14 @@
+﻿#define F_CPU 16000000
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include "robot-mode-rescue.h"
+#include "LCD_Text.h"
+
+void Working_Mode_Rescue(void){
+	
+	// Debugging
+	lcdClear();
+	lcdString(0, 0, "This is Rescue Mode");
+	
+}
---
+++ ProjectMindeList/MindeList/robot-mode-rescue.h
@@ -0,0 +1,6 @@
+﻿#ifndef ROBOT_MODE_RESCUE_H_
+#define ROBOT_MODE_RESCUE_H_
+
+void Working_Mode_Rescue(void);
+
+#endif /* ROBOT_MODE_RESCUE_H_ */
