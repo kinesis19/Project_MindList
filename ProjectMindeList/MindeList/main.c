6402d6b54d24658e0ae863db186228ee6991fc33
Kinesis <lordkinesis@naver.com>
2024-08-29 01:08:47 +0900

    :sparkles: Add Moving Forward and Backward feature in main.c

--- ProjectMindeList/MindeList/main.c
+++ ProjectMindeList/MindeList/main.c
@@ -10,34 +10,37 @@ void Initializing(void);
 int main(void) {
 
     Initializing();
-    lcdString(0, 0, "Hello, MindList");
+	
     while (1) {
 		
     }   
 }
 
+// Timer/Counter1의 초기화
 void Initializing(void){
-    // -----[Declare Register]-----
+	// -----[Declare Register]-----
 	// -----[Initializing: PORT]-----
-    DDRB = 0x6F;  // PB1, PB2 핀을 출력으로 설정하기(OC1A, OC1B).
-    DDRE = 0x38;  // PE3, PE4, PE5 핀을 출력으로 설정하기(OC3A, OC3B, OC3C).
-    
-    // -----[Initializing: Interrupt]-----
-    EIMSK = 0b00000011;
-    EICRA = 0b00001010;
-    sei();
+	DDRB = 0x6F;  // PB5, PB6 PIN을 출력으로 설정하기(OC1A, OC1B). -> DC Motor.
+	DDRE = 0x38;  // PE3, PE4, PE5 핀을 출력으로 설정하기(OC3A, OC3B, OC3C). -> Servo Motor.
 	
-    // -----[Initializing: Timer/Count]-----
-    TCCR1A = 0xA2; // 0b10100010
-    
-    ICR1 = 399;
-    TCNT1 = 0x00;
-    
+	// -----[Initializing: Interrupt]-----
+	EIMSK = 0b00000011; // Switch는 두 개.
+	EICRA = 0b00001010;
+	sei();
 	
-    // -----[Initializing: LCD]-----
-    lcdInit();
-    lcdClear();
+	// -----[Initializing: Timer/Count]-----
+	TCCR1A = 0xA2; // 0b10100010, Fast PWM 모드, 비반전 출력하기.
+	TCCR1B = 0x1A; // 0b00011010, Prescaler 8, Fast PWM 모드.
+	TCCR1C = 0x00;
+	
+	ICR1 = 399;   // TOP 값 설정, PWM 주기 결정
+	TCNT1 = 0x00;
+	
+	// -----[Initializing: LCD]-----
+	lcdInit();
+	lcdClear();
 }
+
 
 // -----[Switch Interrupt Control]-----
 // -----[Moving: Forward]-----
