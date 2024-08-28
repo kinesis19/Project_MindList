b824a01a2d79726271a9d3481f328941743345ec
Kinesis <lordkinesis@naver.com>
2024-08-29 05:09:35 +0900

    :sparkles: Add Buzzer feature and RGB LED feature in robot-mode-army.c
    buzzer 정상 작동 확인 완료.
    RGB LED Sensor는 사용하기 어려움.
    현재는 초록색이 발산됨.

--- ProjectMindeList/MindeList/robot-mode-army.c
+++ ProjectMindeList/MindeList/robot-mode-army.c
@@ -9,13 +9,29 @@ void Working_Mode_Army(void){
 	
 	Mode_Army_Initializing(); // Mode Army Initializing하기.
 	OCR3C = 1800; // 180도 회전하기(수동으로 찾음)
+	
 	// Debugging
 	lcdClear();
 	lcdString(0, 0, "Army Mode");
 	
+	// Buzzer System
+	while (1) {
+		PORTG |= (1 << PG0);
+		_delay_ms(500);
+		PORTG &= ~(1 << PG0);
+		_delay_ms(500);
+	 }
 }
 
 void Mode_Army_Initializing(void){
+	
+	DDRG |= (1 << PG0); // Buzzer
+	DDRA = 0b01010100; // RGB LED
+	
+	// 0b1000000; // RGB LED: Green Color
+	// 0b0010100; // RGB LED: White ~ Blue Color
+	PORTA = 0b10111011; //
+	
 	DDRE |= (1<<PE5);  // PE5 PIN을 Output Mode로 설정하기 (OC3C PIN)
 	 
 	// Servo Motor 같은 경우에는 10개 넘는 레퍼런스랑 데이터 시트 보면서 공부하고 참고함.
