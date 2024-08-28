77c9f8efdd372dce559744288ba809455f3935b0
Kinesis <lordkinesis@naver.com>
2024-08-29 02:10:52 +0900

    :hammer: Create robot-mode-army.c and robot-mode-army.h

---
+++ ProjectMindeList/MindeList/robot-mode-army.c
@@ -0,0 +1,14 @@
+﻿#define F_CPU 16000000
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include "robot-mode-army.h"
+#include "LCD_Text.h"
+
+void Working_Mode_Army(void){
+	
+	// Debugging
+	lcdClear();
+	lcdString(0, 0, "This is Army Mode");
+	
+}
---
+++ ProjectMindeList/MindeList/robot-mode-army.h
@@ -0,0 +1,6 @@
+﻿#ifndef ROBOT_MODE_ARMY_H_
+#define ROBOT_MODE_ARMY_H_
+
+void Working_Mode_Army(void);
+
+#endif /* ROBOT_MODE_ARMY_H_ */
